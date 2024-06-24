132ab2762a13f5d1f21d93a9caa251f1658dab5a
ABT Aurelien <aurelien@abrassart.net>
2024-06-24 10:30:34 +0200

    Ajoutez des fichiers projet.

---
+++ Week4_Q_List/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week4_Q_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancie une liste de chaînes de caractères
+            List<string> listChar = new List<string>();
+
+            // Insères-y trois chaînes de caractères
+            listChar.Add("bonjour");
+            listChar.Add("à");
+            listChar.Add("tous");
+            listChar.ForEach(x => Console.WriteLine(x));
+
+            // Supprime la première avec RemoveAt([index])
+            listChar.RemoveAt(0);
+            // Supprime la troisième avec Remove([valeur])
+            listChar.RemoveAt(1);
+            // Modifie la deuxième en utilisant son indice.
+            listChar[0] = "Bye !";
+            listChar.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
